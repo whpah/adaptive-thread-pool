2d2cae7a8e55242d4c767dcb8895134fbec0e391
HaipengWang <1390925011@qq.com>
2024-12-04 15:27:26 +0800

    fead:搭建基础模块

---
+++ adaptive-thread-pool-spring-boot-starter/src/main/java/com/adaptive/thread/pool/sdk/config/AdaptiveThreadPoolAutoConfig.java
@@ -0,0 +1,11 @@
+package com.adaptive.thread.pool.sdk.config;
+
+/**
+ * @author Haipeng Wang
+ * @description 动态配置入口
+ * @date 2024/12/4/15:24
+ */
+public class AdaptiveThreadPoolAutoConfig {
+
+
+}
---
+++ adaptive-thread-pool-spring-boot-starter/src/test/java/com/adaptive/thread/pool/ApiTest.java
@@ -0,0 +1,9 @@
+package com.adaptive.thread.pool;
+
+/**
+ * @author Haipeng Wang
+ * @description 单元测试
+ * @date 2024/12/4/15:26
+ */
+public class ApiTest {
+}
